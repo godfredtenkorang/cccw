a8bd2a730bf293e61925ca61032d7374a842911d
godeytech <godfredtenkorang9@gmail.com>
2023-07-02 21:05:25 +0000

    added event model

--- church/models.py
+++ church/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class HomeEvent(models.Model):
+    image = models.ImageField(upload_to='home-event-img')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    organizer = models.CharField(max_length=50)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now)
+    time = models.DateTimeField(default=timezone.now)
+    venue = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    slug = models.CharField(max_length=150)
+    
+    class Meta:
+        ordering = ['-start_date']
+    
+    def __str__(self):
+        return self.title
+    
+class Event(models.Model):
+    image = models.ImageField(upload_to='home-event-img')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    organizer = models.CharField(max_length=50)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now)
+    time = models.DateTimeField(default=timezone.now)
+    venue = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    slug = models.CharField(max_length=150)
+    
+    class Meta:
+        ordering = ['-start_date']
+    
+    def __str__(self):
+        return self.title
--- church/views.py
+++ church/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'church/home.html')
+    events = HomeEvent.objects.all()
+    context = {
+        'events': events
+    }
+    return render(request, 'church/home.html', context)
 
 def about(request):
     return render(request, 'church/about.html')
 
 def event(request):
-    return render(request, 'church/event.html')
+    myevents = Event.objects.all()
+    context = {
+        'myevents': myevents,
+        'title': 'Events'
+    }
+    return render(request, 'church/event.html', context)
+
+def event_view(request, slug):
+    myevent = Event.objects.get(slug=slug)
+    context = {
+        'myevent': myevent,
+        'title': 'Event Detail'
+    }
+    return render(request, 'church/event_view.html', context)
+
+def event_detail(request, slug):
+    event = HomeEvent.objects.get(slug=slug)
+    context = {
+        'event': event,
+        'title': 'Event Detail'
+    }
+    return render(request, 'church/event_detail.html', context)
 
 def gallery(request):
     return render(request, 'church/gallery.html')
@@ -26,7 +53,20 @@ def blog_detail(request):
     return render(request, 'church/blog_detail.html')
 
 def contact(request):
-    return render(request, 'church/contact.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        contact = Contact(name=name, email=email, phone=phone, subject=subject, message=message)
+        contact.save()
+        messages.success(request, "Your form is submitted")
+        return redirect('contact')
+    context = {
+        'title': 'Contact us'
+    }
+    return render(request, 'church/contact.html', context)
 
 def donate(request):
     return render(request, 'church/donate.html')
