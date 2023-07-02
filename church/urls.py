e529ba283129f6ca769a25bfffd9409c9defc403
godeytech <godfredtenkorang9@gmail.com>
2023-07-02 17:20:12 +0000

    second commit

--- church/urls.py
+++ church/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('event/', views.event, name='event'),
     path('gallery/', views.gallery, name='gallery'),
     path('youtube/', views.youtube, name='youtube'),
+    path('ministry/', views.ministry, name='ministry'),
+    path('blog/', views.blog, name='blog'),
+    path('blog_detail/', views.blog_detail, name='blog-detail'),
+    path('contact/', views.contact, name='contact'),
+    path('donate/', views.donate, name='donate'),
 ]
--- church/views.py
+++ church/views.py
@@ -13,6 +13,20 @@ def event(request):
 def gallery(request):
     return render(request, 'church/gallery.html')
 
-
 def youtube(request):
     return render(request, 'church/youtube.html')
+
+def ministry(request):
+    return render(request, 'church/ministry.html')
+
+def blog(request):
+    return render(request, 'church/blog.html')
+
+def blog_detail(request):
+    return render(request, 'church/blog_detail.html')
+
+def contact(request):
+    return render(request, 'church/contact.html')
+
+def donate(request):
+    return render(request, 'church/donate.html')
